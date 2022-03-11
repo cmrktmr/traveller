83d3980e45c83dd8861569ee7b597ab618bd9f84
cemreaktemur <cemre.aktemur@gmail.com>
2022-03-12 00:16:20 +0300

    components structure added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 
+import { CssBaseline, Grid } from "@material-ui/core";
+
+//components 
+import Header from "./components/Header/Header";
+import List from "./components/List"
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 
-const App = () =>{
-    return(
-        <div>
-            <h1>Hİ</h1>
-        </div>
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                    <Grid item xs={12} md={4}>
+                        <List></List>
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <PlaceDetails/>
+                    </Grid>
+
+            </Grid>
+        </>
     )
 }
 
